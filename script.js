91e8be591dd61fb13af2c35c3e27470bf78a8454
chatlada07 <86917541+chatlada07@users.noreply.github.com>
2022-01-12 22:35:35 +0700

    Add Subject 2/2562- 2/2563, Edit Checkbox

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function myFunction() {
   // Get the checkbox
-  var checkBox = document.getElementById("myCheck");
+  var checkBox = document.getElementById("myCheck01");
   // Get the output text
-  var text = document.getElementById("text");
+  var text = document.getElementById("text01");
 
   // If the checkbox is checked, display the output text
   if (checkBox.checked == true){
@@ -10,4 +10,38 @@ function myFunction() {
   } else {
     text.style.display = "none";
   }
+ // Get the checkbox
+ var checkBox = document.getElementById("myCheck02");
+ // Get the output text
+ var text = document.getElementById("text02");
+
+ // If the checkbox is checked, display the output text
+ if (checkBox.checked == true){
+   text.style.display = "block";
+ } else {
+   text.style.display = "none";
+ }
+// Get the checkbox
+var checkBox = document.getElementById("myCheck03");
+// Get the output text
+var text = document.getElementById("text03");
+
+// If the checkbox is checked, display the output text
+if (checkBox.checked == true){
+  text.style.display = "block";
+} else {
+  text.style.display = "none";
+}
+// Get the checkbox
+var checkBox = document.getElementById("myCheck04");
+// Get the output text
+var text = document.getElementById("text04");
+
+// If the checkbox is checked, display the output text
+if (checkBox.checked == true){
+  text.style.display = "block";
+} else {
+  text.style.display = "none";
+}
+
 }
